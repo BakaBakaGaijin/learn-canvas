6678ea9b3475348f0d582007cb6ff0968fe1b277
BakaBakaGaijin <legostaevofficial@yandex.ru>
2023-10-14 17:44:40 +0300

    fillStyle

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ function draw() {
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
 
-    const p = new Path2D("M 10 10 h 80 v 80 h -80 Z");
-    ctx.fill(p);
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        ctx.fillStyle =
+          "rgb(" +
+          Math.floor(255 - 42.5 * i) +
+          "," +
+          Math.floor(255 - 42.5 * j) +
+          ",0)";
+        ctx.fillRect(j * 25, i * 25, 25, 25);
+      }
+    }
+
+    /* const p = new Path2D("M 10 10 h 80 v 80 h -80 Z");
+    ctx.fill(p); */
 
     /* const rectangle = new Path2D();
     rectangle.rect(10, 10, 50, 50);
