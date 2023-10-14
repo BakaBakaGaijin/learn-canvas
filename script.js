9d7c90f9728b5740a686c1f9cd5e2166c0bc8465
BakaBakaGaijin <legostaevofficial@yandex.ru>
2023-10-14 16:22:28 +0300

    Rectangles

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ function draw() {
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
 
-    ctx.beginPath();
-    ctx.moveTo(75, 40);
-    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.rect(0, 0, 100, 100);
     ctx.fill();
+    // ctx.stroke();
+
+    // ctx.beginPath();
+    // ctx.moveTo(75, 40);
+    // ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    // ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+    // ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+    // ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+    // ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+    // ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    // ctx.fill();
 
     /* ctx.beginPath();
     ctx.moveTo(75, 25);
