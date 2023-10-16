900b56592d808fa00be7ddb8a9580c177b9122d2
BakaBakaGaijin <legostaevofficial@yandex.ru>
2023-10-16 23:27:07 +0300

    createLinearGradient

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 const canvas = document.body.querySelector("#tutorial");
-console.log("canvas: ", canvas);
 
 if (canvas.getContext) {
   const ctx = canvas.getContext("2d");
 
-  let offset = 0;
+  const radgrad = ctx.createRadialGradient(45, 45, 10, 52, 50, 30);
+  radgrad.addColorStop(0, "#A7D30C");
+  radgrad.addColorStop(0.9, "#019F62");
+  radgrad.addColorStop(1, "rgba(1,159,98,0)");
+
+  const radgrad2 = ctx.createRadialGradient(105, 105, 20, 112, 120, 50);
+  radgrad2.addColorStop(0, "#FF5F98");
+  radgrad2.addColorStop(0.75, "#FF0188");
+  radgrad2.addColorStop(1, "rgba(255,1,136,0)");
+
+  const radgrad3 = ctx.createRadialGradient(95, 15, 15, 102, 20, 40);
+  radgrad3.addColorStop(0, "#00C9FF");
+  radgrad3.addColorStop(0.8, "#00B5E2");
+  radgrad3.addColorStop(1, "rgba(0,201,255,0)");
+
+  const radgrad4 = ctx.createRadialGradient(0, 150, 50, 0, 140, 90);
+  radgrad4.addColorStop(0, "#F4F201");
+  radgrad4.addColorStop(0.8, "#E4C700");
+  radgrad4.addColorStop(1, "rgba(228,199,0,0)");
+
+  ctx.fillStyle = radgrad4;
+  ctx.fillRect(0, 0, 150, 150);
+  ctx.fillStyle = radgrad3;
+  ctx.fillRect(0, 0, 150, 150);
+  ctx.fillStyle = radgrad2;
+  ctx.fillRect(0, 0, 150, 150);
+  ctx.fillStyle = radgrad;
+  ctx.fillRect(0, 0, 150, 150);
+
+  /* const lingrad = ctx.createLinearGradient(0, 0, 0, 150);
+  lingrad.addColorStop(0, "#00ABEB");
+  lingrad.addColorStop(0.5, "#fff");
+  lingrad.addColorStop(0.5, "#26c000");
+  lingrad.addColorStop(1, "#fff");
+
+  const lingrad2 = ctx.createLinearGradient(0, 0, 0, 95);
+  lingrad2.addColorStop(0.5, "#000");
+  lingrad2.addColorStop(1, "rgba(0,0,0,0)");
+
+  ctx.fillStyle = lingrad;
+  ctx.strokeStyle = lingrad2;
+
+  ctx.fillRect(10, 10, 130, 130);
+  ctx.strokeRect(50, 50, 50, 50); */
+
+  /* let offset = 0;
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -23,7 +67,7 @@ if (canvas.getContext) {
     setTimeout(march, 20);
   }
 
-  march();
+  march(); */
 
   /* // Clear canvas
   ctx.clearRect(0, 0, 150, 150);
