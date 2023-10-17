84bd3407ba309f0afd9115393cc5852c4901bc77
BakaBakaGaijin <legostaevofficial@yandex.ru>
2023-10-17 22:03:22 +0300

    Shadow

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const canvas = document.body.querySelector("#tutorial");
 if (canvas.getContext) {
   const ctx = canvas.getContext("2d");
 
+  ctx.shadowOffsetX = 2;
+  ctx.shadowOffsetY = 2;
+  ctx.shadowBlur = 2;
+  ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
+
+  ctx.font = "20px Times New Roman";
+  ctx.fillStyle = "Black";
+  ctx.fillText("Sample String", 5, 30);
+
   /* const img = new Image(50, 50);
   img.src = "image.avif";
   img.onload = function () {
